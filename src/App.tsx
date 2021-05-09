2729b5616c9068f28aeb1eeda968dd8039bcde27
Sukhdev <ssbanwait78@gmail.com>
2021-05-09 09:59:31 -0400

    styling and refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,87 @@
-import React from 'react';
-import './App.css';
-import SearchBar from './components/search-bar/main';
+import React, { useCallback, useEffect, useRef, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { useDebounce } from './app/hooks'
+import { useTypedSelector } from './app/store'
+import { fetchMovies, selectMovies, selectPageNumber, selectStatus, selectTotalPages } from './features/movie-results/movieResultsSlice'
+import { Movie } from './features/movie/movie'
+import { Nominations } from './features/nominations/Nominations'
+import MovieCard from './components/movie-card/MovieCard'
+import './App.scss'
 
 function App() {
-  return (
-    <div className="App">
-      <SearchBar />
-    </div>
-  );
+    const dispatch = useDispatch()
+    const [input, setInput] = useState('')
+    const debouncedSearchTerm: string = useDebounce<string>(input, 500);
+    const status = useTypedSelector(selectStatus)
+    const movies: Movie[] = useTypedSelector(selectMovies)
+    const observer = useRef<HTMLDivElement | IntersectionObserver | null>(null)
+    const totalPages = useTypedSelector(selectTotalPages)
+    const currentPage = useTypedSelector(selectPageNumber)
+    const lastElementRef = useCallback((node) => {
+        if (status === 'loading') return
+        console.table({ totalPages, currentPage })
+        // if (observer.current) observer.current.disconnect()
+        if (observer.current) {
+            (observer.current as IntersectionObserver).disconnect()
+        }
+        observer.current = new IntersectionObserver(entries => {
+            const check = currentPage != totalPages
+            if (entries[0].isIntersecting && check) {
+                console.log('should fetch more movies')
+                dispatch(fetchMovies({ selection: input, page: currentPage + 1 }))
+            }
+            if (entries[0].isIntersecting) {
+                console.log('intersection!')
+            }
+            else console.log('no intersection')
+        })
+        if (node) {
+            observer.current.observe(node)
+        }
+    }, [currentPage, status])
+
+
+
+    // Effect for API call
+    useEffect(
+        () => {
+            if (debouncedSearchTerm) {
+                dispatch(fetchMovies({ selection: input, page: 1 }))
+            } else {
+                dispatch(fetchMovies({ selection: '', page: 1 })) // reset list
+            }
+        },
+        [debouncedSearchTerm] // Only call effect if debounced search term changes
+    );
+
+    return (
+        <>
+            <section className='navbar'>
+                <h1 className='navbar_title'>The Shoppies</h1>
+                <input className='navbar_input' type='text' value={input} placeholder='Search movie...'
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => setInput(e.target.value)} />
+                <section className='navbar_status-indicator'>{status}</section>
+            </section>
+            <Nominations />
+            <section className='search-results'>
+                <h3 className='search-results_title'> Search Results </h3>
+                <ul className='search-results_movie-list'>
+                    {movies.map((movie: Movie, index) => {
+                        if (movies.length === index + 1) {
+                            console.log('attached ref')
+                            return (<li ref={lastElementRef} key={movie.imdbID} className='movie'>
+                                <MovieCard {...movie} />
+                            </li>)
+                        }
+                        return (<li key={movie.imdbID} className='movie'>
+                            <MovieCard {...movie} />
+                        </li>)
+                    })}
+
+                </ul>
+            </section>
+        </>
+    )
 }
 
-export default App;
+export default App
--- src/components/movie-card/MovieCard.tsx
+++ src/components/movie-card/MovieCard.tsx
@@ -4,21 +4,30 @@ import { addNomination, selectNoms } from '../../features/nominations/nomination
 import { useDispatch } from 'react-redux';
 import { useTypedSelector } from '../../app/store';
 
-// const MovieCard = ({ Title, Year, Poster, imdbID }: Movie) => {
 const MovieCard = (movie: Movie) => {
     const dispatch = useDispatch()
     const nominees = useTypedSelector(selectNoms)
     const { Title, Year, Poster, imdbID } = movie
     let isNominated = false
-    const index = nominees.find(m => m.imdbID === movie.imdbID)
+    const index = nominees.find(m => m.imdbID === imdbID)
     if (index) {
         isNominated = true
     }
 
     return (
-        <section>
-            {Title}
-            {/* <img src={Poster} /> */}
+        <section className='movie-card'>
+            <section className="movie-card_img-container">
+                <img alt='no photo available' className='movie-card_img-container_img' src={Poster} />
+            </section>
+            <section className="movie-card_details">
+                <h3 className='movie-card_details_title'>
+                    {Title}
+                </h3>
+                <h4 className='movie-card_details_year'>
+                    {Year}
+                </h4>
+
+            </section>
             <button disabled={isNominated} onClick={() => dispatch(addNomination(movie))}>Nominate </button>
         </section>
     )
