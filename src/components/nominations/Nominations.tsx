bf82edbc52d4f7a655b5bb653a9e3e560fd5ff35
Sukhdev <ssbanwait78@gmail.com>
2021-05-11 14:10:23 -0400

    style changes

--- src/components/nominations/Nominations.tsx
+++ src/components/nominations/Nominations.tsx
@@ -11,15 +11,19 @@ export function Nominations() {
 
     return (
         <section className='nominations'>
-
             <h3 className='nominations_title'>
                 Nominations
             </h3>
-            <ul className='nominations_movie-list'>
-                {movies.map((movie: Movie) => <li key={movie.imdbID}
-                    className='movie'><NominationCard {...movie} /></li>)}
-            </ul>
-
+            {
+                movies.length ?
+                    <ul className='nominations_movie-list'>
+                        {movies.map((movie: Movie) => <li key={movie.imdbID}
+                            className='movie'><NominationCard {...movie} /></li>)}
+                    </ul> :
+                    <h4 className='nominations_no-noms'>
+                        First search for a movie, and then nominate!
+                    </h4>
+            }
         </section>
     );
 }
